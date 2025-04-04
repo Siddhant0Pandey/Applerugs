9eca885c56902820f9e5b8423853fe1c7ae9e849
Siddhant0Pandey <pandeysiddhant0922@gmail.com>
2025-04-04 14:11:08 +0545

    contact form updated

--- contact.php
+++ contact.php
@@ -1,10 +1,9 @@
 <?php
 
-if(!$_POST) exit;
+if (!$_POST) exit;
 
-// Email address verification, do not edit.
 function isEmail($email) {
-	return(preg_match("/^[-_.[:alnum:]]+@((([[:alnum:]]|[[:alnum:]][[:alnum:]-]*[[:alnum:]])\.)+(ad|ae|aero|af|ag|ai|al|am|an|ao|aq|ar|arpa|as|at|au|aw|az|ba|bb|bd|be|bf|bg|bh|bi|biz|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|com|coop|cr|cs|cu|cv|cx|cy|cz|de|dj|dk|dm|do|dz|ec|edu|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gh|gi|gl|gm|gn|gov|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|in|info|int|io|iq|ir|is|it|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mil|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|museum|mv|mw|mx|my|mz|na|name|nc|ne|net|nf|ng|ni|nl|no|np|nr|nt|nu|nz|om|org|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|pro|ps|pt|pw|py|qa|re|ro|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|sk|sl|sm|sn|so|sr|st|su|sv|sy|sz|tc|td|tf|tg|th|tj|tk|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|um|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)$|(([0-9][0-9]?|[0-1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5])\.){3}([0-9][0-9]?|[0-1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5]))$/i",$email));
+    return(preg_match("/^[-_.[:alnum:]]+@((([[:alnum:]]|[[:alnum:]][[:alnum:]-]*[[:alnum:]])\.)+(ad|ae|aero|af|ag|ai|al|am|an|ao|aq|ar|arpa|as|at|au|aw|az|ba|bb|bd|be|bf|bg|bh|bi|biz|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|com|coop|cr|cs|cu|cv|cx|cy|cz|de|dj|dk|dm|do|dz|ec|edu|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gh|gi|gl|gm|gn|gov|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|in|info|int|io|iq|ir|is|it|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mil|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|museum|mv|mw|mx|my|mz|na|name|nc|ne|net|nf|ng|ni|nl|no|np|nr|nt|nu|nz|om|org|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|pro|ps|pt|pw|py|qa|re|ro|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|sk|sl|sm|sn|so|sr|st|su|sv|sy|sz|tc|td|tf|tg|th|tj|tk|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|um|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)$|(([0-9][0-9]?|[0-1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5])\.){3}([0-9][0-9]?|[0-1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5]))$/i", $email));
 }
 
 if (!defined("PHP_EOL")) define("PHP_EOL", "\r\n");
@@ -12,75 +11,64 @@ if (!defined("PHP_EOL")) define("PHP_EOL", "\r\n");
 $name     = $_POST['name'];
 $email    = $_POST['email'];
 $comments = $_POST['comments'];
-$verify   = $_POST['verify'];
 
-if(trim($name) == '') {
-	echo '<div class="error_message">Attention! You must enter your name.</div>';
-	exit();
-} else if(trim($email) == '') {
-	echo '<div class="error_message">Attention! Please enter a valid email address.</div>';
-	exit();
-} else if(!isEmail($email)) {
-	echo '<div class="error_message">Attention! You have enter an invalid e-mail address, try again.</div>';
-	exit();
-}else if(trim($comments) == '') {
-	echo '<div class="error_message">Attention! Please enter your message.</div>';
-	exit();
-}else if(!isset($verify) || trim($verify) == '') {
-	echo '<div class="error_message">Attention! Please enter the verification number.</div>';
-	exit();
-} else if(trim($verify) != '4') {
-	echo '<div class="error_message">Attention! The verification number you entered is incorrect.</div>';
-	exit();
-} 
+if (trim($name) == '') {
+    echo '<div class="error_message">Attention! You must enter your name.</div>';
+    exit();
+} else if (trim($email) == '') {
+    echo '<div class="error_message">Attention! Please enter a valid email address.</div>';
+    exit();
+} else if (!isEmail($email)) {
+    echo '<div class="error_message">Attention! You have entered an invalid e-mail address, try again.</div>';
+    exit();
+} else if (trim($comments) == '') {
+    echo '<div class="error_message">Attention! Please enter your message.</div>';
+    exit();
+}
 
 $comments = htmlspecialchars($comments);
 
+// Load configuration from the config file
+$config = require 'config.php'; 
 
-// Configuration option.
-// Enter the email address that you want to emails to be sent to.
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-//$address = "example@youremail.net";
-$address = "clapatform@gmail.com";
+require 'vendor/autoload.php';
 
+$mail = new PHPMailer(true);
 
-// Configuration option.
-// i.e. The standard subject will appear as, "You've been contacted by John Doe."
+try {
+    //Server settings
+    $mail->isSMTP();                                      
+    $mail->Host = $config['smtp_host'];                 
+    $mail->SMTPAuth = true;                            
+    $mail->Username = $config['smtp_username'];         
+    $mail->Password = $config['smtp_password'];         
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;          
+    $mail->Port = $config['smtp_port'];                
 
-// Example, $e_subject = '$name . ' has contacted you via Your Website.';
+    //Recipients
+    $mail->setFrom($email, $name);                             
+    $mail->addAddress($config['to_email']);              
 
-$e_subject = 'You\'ve been contacted by ' . $name . '.';
+    // Content
+    $mail->isHTML(false);                                 
+    $mail->Subject = 'You\'ve been contacted by ' . $name;
+    $mail->Body = "You have been contacted by $name, their additional message is as follows." . PHP_EOL . PHP_EOL;
+    $mail->Body .= "\"$comments\"" . PHP_EOL . PHP_EOL;
+    $mail->Body .= "You can contact $name via email, $email";
 
-
-// Configuration option.
-// You can change this if you feel that you need to.
-// Developers, you may wish to add more fields to the form, in which case you must be sure to add them here.
-
-$e_body = "You have been contacted by $name, their additional message is as follows." . PHP_EOL . PHP_EOL;
-$e_content = "\"$comments\"" . PHP_EOL . PHP_EOL;
-$e_reply = "You can contact $name via email, $email";
-
-$msg = wordwrap( $e_body . $e_content . $e_reply, 70 );
-
-$headers = "From: $email" . PHP_EOL;
-$headers .= "Reply-To: $email" . PHP_EOL;
-$headers .= "MIME-Version: 1.0" . PHP_EOL;
-$headers .= "Content-type: text/plain; charset=utf-8" . PHP_EOL;
-$headers .= "Content-Transfer-Encoding: quoted-printable" . PHP_EOL;
-
-if(mail($address, $e_subject, $msg, $headers)) {
-
-	// Email has sent successfully, echo a success page.
-
-	echo "<fieldset>";
-	echo "<div id='success_page'>";
-	echo "<h3>Email Sent Successfully.</h3>";
-	echo "<p>Thank you <strong>$name</strong>, your message has been submitted to us.</p>";
-	echo "</div>";
-	echo "</fieldset>";
-
-} else {
-
-	echo 'ERROR!';
-
+    // Send the email
+    $mail->send();
+    echo "<fieldset>";
+    echo "<div id='success_page'>";
+    echo "<h3>Email Sent Successfully.</h3>";
+    echo "<p>Thank you <strong>$name</strong>, your message has been submitted to us.</p>";
+    echo "</div>";
+    echo "</fieldset>";
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
+
+?>
